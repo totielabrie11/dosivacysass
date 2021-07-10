a26e133903900c6a2604a84c6519cbe1d9e145ca
totielabrie11 <martinezcarlosmanuel.1@gmail.com>
2021-07-10 01:01:10 -0300

    mejoras notables en toda la app del selector

--- js/function.js
+++ js/function.js
@@ -1,34 +1,39 @@
 //funcion constructora -> para caputrar los datos de la seleccion
-function capturar(){
-    function Selection(nombre, linea, modelo, precio){
-        this.nombre=(nombre.toUpperCase());
-        this.linea=linea;
-        this.modelo=modelo;
-        precio= precio * 1.21;  
-        this.precio=precio;
-    } 
-    let nombreCapturar = tipoDeProceso;
-    let lineaCapturar = linea;
-    let modeloCapturar = modelo;
-    let precioCapturar = precio;  
+function capturar() {
+  function Selection(nombre, linea, modelo, precio, img) {
+    this.nombre = nombre.toUpperCase();
+    this.linea = linea;
+    this.modelo = modelo;
+    precio = precio * 1.21;
+    this.precio = precio;
+    this.img = img;
+  }
+  let nombreCapturar = tipoDeProceso;
+  let lineaCapturar = linea;
+  let modeloCapturar = modelo;
+  let precioCapturar = precio;
+  let imgCapturar = img;
 
-    contador = new Selection(nombreCapturar, lineaCapturar, modeloCapturar, precioCapturar); 
-     
-    arraySelection = [];
-    arraySelection.push(contador);
-    console.log(arraySelection); 
+  contador = new Selection(
+    nombreCapturar,
+    lineaCapturar,
+    modeloCapturar,
+    precioCapturar,
+    imgCapturar
+  );
+
+  arraySelection = [];
+  arraySelection.push(contador);
+  console.log(arraySelection);
 }
 
 const GuardarLS = () => {
+  localStorage.setItem("selection", JSON.stringify(arraySelection));
+};
 
-    localStorage.setItem('selection', JSON.stringify(arraySelection));
-    
-}
-
-const EliminarLS = (consulta) => { 
-
-    console.log(consulta);
-    /* let indexArray;
+const EliminarLS = (consulta) => {
+  console.log(consulta);
+  /* let indexArray;
     arraySelection.forEach((elemento, index) => {
         if(elemento.nombre === nombre){
             indexArray = index;   
@@ -37,61 +42,48 @@ const EliminarLS = (consulta) => {
     
     arraySelection.splice(indexArray,1);
     GuardarLS(); */
-}
+};
 
 const PintarDom = () => {
-    
-    listaSelector.innerHTML = '';
+  resultadoSelector.innerHTML = "";
 
-    arraySelection = JSON.parse(localStorage.getItem('selection'));
-    console.log(arraySelection);// no importa el null por que entra igual con el if;
+  arraySelection = JSON.parse(localStorage.getItem("selection"));
+  console.log(arraySelection); // no importa el null por que entra igual con el if;
 
-    if(arraySelection === null){
-        arraySelection = [];
-    }else {
-        arraySelection.forEach(element => {
-
-            listaSelector.innerHTML += `<tbody id="listaSelector" class="text-warning alert-danger"><td>${contador.nombre}</td><td>${contador.linea}</td><td>${contador.modelo}</td><td><i class="material-icons">attach_money</i>${contador.precio}</td><td><i class="material-icons me-5" >delete</i><i class="material-icons">favorite</i></td></tbody>`;
-
-            if (linea === null){
-
-               tabla.innerHTML += `<div class="alert alert-danger float-end" role="alert">SU BUSQUEDA NO HA ENCONTRADO RESULTADO</div>`;
-                
-            }
-
-        });
-    }
-}
-
+  if (arraySelection === null) {
+    arraySelection = [];
+  } else {
+    if (linea === null) {
+      tabla.innerHTML += `<div class="alert alert-danger float-end" role="alert">SU BUSQUEDA NO HA ENCONTRADO RESULTADO</div>`;
+      return;
+    } else
+      arraySelection.forEach((element) => {
+        resultadoSelector.innerHTML += `<div id="resultadoSelector" class="text-dark lista__tabla"><ul><li>'${contador.nombre}'</li><li>'${contador.linea}'</li><li>'${contador.modelo}'</li><li><span class="material-icons w-50">attach_money</span>'${contador.precio}'</li><li> <i class="material-icons" >delete</i></li><li><i class="material-icons">favorite</i></li></ul><hr><section class="d-flex justify-content-between" style="height: 20rem;"><div><h4>CARACTERISTICAS</h4><ol><li>hola</li><li>chau</li><li></li></ol></div><div style="border-left:1px solid rgba(105, 103, 103, 0.322)"></div><div><h4 class="text-center">DISEÑO</h4><div class="d-flex justify-content-center">'${contador.img}'</div></div></section></div>`;
+      });
+  }
+};
 //Eventos del DOM
 
 //En este momento puedo cargar todas mis funciones en orden, queda mucho mas prolijo, se ve el orden de ejecución. a la espera del submit;
 
-form.addEventListener('submit', (e)=> {
+/* form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    e.preventDefault();
-    
-    GuardarLS();
+  GuardarLS();
+}); */
 
-    PintarDom();
+document.addEventListener("DOMContentLoaded", PintarDom);
 
-});
+resultadoSelector.addEventListener("click", (e) => {
+  e.preventDefault();
 
-document.addEventListener('DOMContentLoaded', PintarDom);
+  console.log(e.target.innerHTML);
 
-listaSelector.addEventListener('click', (e) => {
-
-    e.preventDefault();
-    
-    console.log(e.target.innerHTML);
-
-    if (e.target.innerHTML === 'delete' || e.target.innerHTML === 'favorite'){
-        
-        if(e.target.innerHTML === 'delete'){
-            EliminarLS(e.path[2].childNodes[4].innerHTML);
-        }
-    }if(e.target.innerHTML === 'favorite'){
-
+  if (e.target.innerHTML === "delete" || e.target.innerHTML === "favorite") {
+    if (e.target.innerHTML === "delete") {
+      EliminarLS(e.path[2].childNodes[4].innerHTML);
     }
+  }
+  if (e.target.innerHTML === "favorite") {
+  }
 });
-    
--- js/js.js
+++ js/js.js
@@ -1,7 +1,8 @@
-let linea = '';
-let modelo = '';
+let linea = "";
+let modelo = "";
 let precio = 0;
-tipoDeProceso = '';
+let img = "";
+tipoDeProceso = "";
 let desplazamiento = 0;
 let caudal = 0;
 let contrapresion = 0;
@@ -9,29 +10,28 @@ let contrapresion = 0;
 const arrayDeConsultas = [];
 
 let inputValues = {
+  proceso: "",
+  desplazamiento: Number(),
+  caudal: Number(),
+  contrapresion: Number(),
+};
 
-    proceso: '',
-    desplazamiento: Number(),
-    caudal: Number(),
-    contrapresion: Number()
+const handleForm = (e) => {
+  //eventos del DOM
+  e.preventDefault();
 
-}
-
-const handleForm = e => {
-    e.preventDefault();
-
-    if (
-        inputValues.proceso.trim() === '' ||
-        inputValues.desplazamiento.trim() === '' ||
-        inputValues.caudal.trim() === '' ||
-        inputValues.contrapresion.trim() === ''
-    ) {
-    console.log('Todos los campos deben ser completados');
+  if (
+    inputValues.proceso.trim() === "" ||
+    inputValues.desplazamiento.trim() === "" ||
+    inputValues.caudal.trim() === "" ||
+    inputValues.contrapresion.trim() === ""
+  ) {
+    console.log("Todos los campos deben ser completados");
     return; //recuerden que este return lo ponemos para evitar que la funcion siga ejecutandose
   }
 
   arrayDeConsultas.push(inputValues);
-  console.log('Despues de pushear los datos: ', arrayDeConsultas);
+  console.log("Despues de pushear los datos: ", arrayDeConsultas);
 
   e.preventDefault();
   capturar();
@@ -39,238 +39,243 @@ const handleForm = e => {
   PintarDom();
 };
 
+const handleInput = (e) => {
+  //Aqui viene lo "complejo", necesitamos hacer este formulario dinamico, como hacemos eso? capturando el e.target.name del elemento y buscar que coincida con alguna propiedad del objeto inputValues, es por eso que es importante que los nombres de las propiedades del objeto sean las mismas que la de los names en el html. Por ejemplo e.target.name del segundo input sera apellido, este entrara en el objeto y buscara una propiedad que sea igual, si la consigue le agregara el e.target.value que le pasamos
 
-const handleInput = e => {
-    //Aqui viene lo "complejo", necesitamos hacer este formulario dinamico, como hacemos eso? capturando el e.target.name del elemento y buscar que coincida con alguna propiedad del objeto inputValues, es por eso que es importante que los nombres de las propiedades del objeto sean las mismas que la de los names en el html. Por ejemplo e.target.name del segundo input sera apellido, este entrara en el objeto y buscara una propiedad que sea igual, si la consigue le agregara el e.target.value que le pasamos
-  
-    console.log('Soy el input donde estas escribiendo: ', e.target);
-    console.log('Soy el atributo value de ese input: ', e.target.value);
-    console.log('Soy el atributo name de ese input: ', e.target.name);
+  console.log("Soy el input donde estas escribiendo: ", e.target);
+  console.log("Soy el atributo value de ese input: ", e.target.value);
+  console.log("Soy el atributo name de ese input: ", e.target.name);
 
-    
-  
-    inputValues = {
-      ...inputValues, //esos ... se llama spread operator, googlear! Va a devolver una copia del objeto, dejo comentado como se veria el objeto si no tuviera eso
-      [e.target.name]: e.target.value
-    };
-  
-    // inputValues = {
-    //   [e.target.name]: e.target.value
-    // }
-    console.log(inputValues);
-    tipoDeProceso = inputValues.proceso;
-    console.log(tipoDeProceso);
-    desplazamiento = Number(inputValues.desplazamiento);
-    caudal = Number(inputValues.caudal);
-    contrapresion = Number(inputValues.contrapresion);
-    
-    switch (tipoDeProceso) { /* como la variable que determina el proceso a seguir, se declara dentro de la funcion. Tuve que pegar todo el switch dentro de la funcion. Me gustaría poder arreglar esto en futuras entregas */
-        case 'residual':
-            
-            selectorVacioR();
-            
-            break;
-    
-        case 'altovacio':
-            
-            selectorVacioA();
-            
-            break;
-        
-        case "laboratorio":
-            
-            selectorVacioLaboratorio();            
-    
-            break;
-    
-        case 'electrica':
-    
-            selectorDosificadorElectrico();
-            
-            break;
-    
-        case 'neumatica':
-    
-            selectorDosificadorNeumatico();
-                
-            break;
-    
-        case 'solar':
-    
-            selectorDosificadorSolar();
-                
-            break;
-        
-        default: 
-            document.innerHTML + `NO HAY RESULTADOS PARA SU BUSQUEDA`;
-    }
-    
-    
+  inputValues = {
+    ...inputValues, //esos ... se llama spread operator, googlear! Va a devolver una copia del objeto, dejo comentado como se veria el objeto si no tuviera eso
+    [e.target.name]: e.target.value,
+  };
+
+  // inputValues = {
+  //   [e.target.name]: e.target.value
+  // }
+  console.log(inputValues);
+  tipoDeProceso = inputValues.proceso;
+  console.log(tipoDeProceso);
+  desplazamiento = Number(inputValues.desplazamiento);
+  caudal = Number(inputValues.caudal);
+  contrapresion = Number(inputValues.contrapresion);
+
+  switch (
+    tipoDeProceso /* como la variable que determina el proceso a seguir, se declara dentro de la funcion. Tuve que pegar todo el switch dentro de la funcion. Me gustaría poder arreglar esto en futuras entregas */
+  ) {
+    case "residual":
+      selectorVacioR();
+
+      break;
+
+    case "altovacio":
+      selectorVacioA();
+
+      break;
+
+    case "laboratorio":
+      selectorVacioLaboratorio();
+
+      break;
+
+    case "electrica":
+      selectorDosificadorElectrico();
+
+      break;
+
+    case "neumatica":
+      selectorDosificadorNeumatico();
+
+      break;
+
+    case "solar":
+      selectorDosificadorSolar();
+
+      break;
+
+    default:
+      document.innerHTML + `NO HAY RESULTADOS PARA SU BUSQUEDA`;
+  }
 };
 //llamamos el formulario
-const form = document.querySelector('form');
+const form = document.querySelector("form");
 
 //Llamamos a los inputs
-const inputs = document.querySelectorAll('.input');
-console.log(inputs)
+const inputs = document.querySelectorAll(".input");
+console.log(inputs);
+
+//lamamos al alert donde pintaremos nuestra busqueda
+const alertDom = document.querySelector("#resultadoSelector");
+console.log(alertDom);
 
 //hacemos el evento submit del form que va a ejecutar la funcion sendForm
-form.addEventListener('submit', handleForm);
-
+form.addEventListener("submit", handleForm);
 
 //hacemos el evento para capturar los values del input, pero como los inputs lo llamamos con un querySelectorAll tenemos que hacer una iteracion primero
-inputs.forEach(input => input.addEventListener('input', handleInput));
+inputs.forEach((input) => input.addEventListener("input", handleInput));
 
-
-function selectorVacioA(){
-    if (desplazamiento <= 60){
-        
-        linea = "DVRII"; modelo = "1A"; precio = 175; 
-    
-    }else if (desplazamiento <= 130){
-        
-        linea = "DVRII"; modelo = "2A"; precio = 260;
-        
-    }else if (desplazamiento <= 170){
-        
-        linea = "DVRII"; modelo = "3A"; precio = 260;
-        
-    }else if (desplazamiento <= 280){
-    
-    linea = "DVRII"; modelo = "4A"; precio = 380;
-        
-    }else {
-        linea = null;
-    }
-    console.log('llegue hasta selector de alto vacio');
+function selectorVacioA() {
+  if (desplazamiento <= 60) {
+    linea = "DVRII";
+    modelo = "1A";
+    precio = 175;
+  } else if (desplazamiento <= 130) {
+    linea = "DVRII";
+    modelo = "2A";
+    precio = 260;
+  } else if (desplazamiento <= 170) {
+    linea = "DVRII";
+    modelo = "3A";
+    precio = 260;
+  } else if (desplazamiento <= 280) {
+    linea = "DVRII";
+    modelo = "4A";
+    precio = 380;
+  } else {
+    linea = null;
+  }
+  console.log("llegue hasta selector de alto vacio");
+  img =
+    '<img src="../img/bombas/DVR2-removebg-preview.png" width="60%" alt="">';
 }
 
-function selectorVacioR(){
-    if (desplazamiento <= 400){
-       
-        linea = "DSHC"; modelo = "400"; precio = 693;
-
-    }else if (desplazamiento <= 800){
-       
-        linea = "DSHC"; modelo = "800"; precio = 990;
-
-    }else if (desplazamiento <= 1250){
-       
-        linea = "DSHC"; modelo = "1250"; precio = 1420;
-      
-    }else if (desplazamiento <= 1810){
-       
-        linea = "DSHC"; modelo = "1810"; precio = 990;    
-
-    }else if (desplazamiento <= 2500){
-       
-        linea = "DSHC"; modelo = "2500"; precio = 2574;
-
-    }else if (desplazamiento <= 3000){
-       
-            linea = "DSHC"; modelo = "3000"; precio = 3049;
-
-    }else if (desplazamiento <= 4500){
-       
-        linea = "DSHC"; modelo = "4500"; precio = 4139;
-
-    }else if (desplazamiento <= 6300){
-       
-        linea = "DSHC"; modelo = "6300"; precio = 6765;
-    
-    }else {
-        linea = null;
-    }
-    console.log('llegue hasta selector de residual')
+function selectorVacioR() {
+  if (desplazamiento <= 400) {
+    linea = "DSHC";
+    modelo = "400";
+    precio = 693;
+  } else if (desplazamiento <= 800) {
+    linea = "DSHC";
+    modelo = "800";
+    precio = 990;
+  } else if (desplazamiento <= 1250) {
+    linea = "DSHC";
+    modelo = "1250";
+    precio = 1420;
+  } else if (desplazamiento <= 1810) {
+    linea = "DSHC";
+    modelo = "1810";
+    precio = 990;
+  } else if (desplazamiento <= 2500) {
+    linea = "DSHC";
+    modelo = "2500";
+    precio = 2574;
+  } else if (desplazamiento <= 3000) {
+    linea = "DSHC";
+    modelo = "3000";
+    precio = 3049;
+  } else if (desplazamiento <= 4500) {
+    linea = "DSHC";
+    modelo = "4500";
+    precio = 4139;
+  } else if (desplazamiento <= 6300) {
+    linea = "DSHC";
+    modelo = "6300";
+    precio = 6765;
+  } else {
+    linea = null;
+  }
+  console.log("llegue hasta selector de residual");
+  img =
+    '<img src="../img/bombas/DSHC BRIDA-removebg-preview.png" width="60%" alt="">';
 }
 
-function selectorVacioLaboratorio(){
-    if (desplazamiento >= 100 && desplazamiento <= 150){
-    
-        linea = "DVL"; modelo = "DVL150"; precio = 325;
-
-    }else if (desplazamiento >= 1 && desplazamiento <= 100){
-       
-        linea = "DVL"; modelo = "DVL150"; precio = 325;
-    }else {
-        linea = null;
-    }
-    console.log('llegue hasta selector de alto vacio para laboratorio')
+function selectorVacioLaboratorio() {
+  if (desplazamiento >= 100 && desplazamiento <= 150) {
+    linea = "DVL";
+    modelo = "DVL150";
+    precio = 325;
+  } else if (desplazamiento >= 1 && desplazamiento <= 100) {
+    linea = "DVL";
+    modelo = "DVL150";
+    precio = 325;
+  } else {
+    linea = null;
+  }
+  console.log("llegue hasta selector de alto vacio para laboratorio");
 }
 
-function selectorDosificadorElectrico(){
-    if (contrapresion <= 10 && caudal >=16 && caudal <=30) {
-        
-        linea = "DDI"; modelo = "30"; precio = 1250;
-
-    }else if (contrapresion <= 10 && caudal >=30 && caudal <=60) {
-            
-            linea = "DDI"; modelo = "60"; precio = 1250;
-
-    }else if (contrapresion <= 10 && caudal >=60 && caudal <=80) {
-           
-            linea = "DDI"; modelo = "80"; precio = 1250;
-
-    }else if (contrapresion <= 10 && caudal >=80 && caudal <=150) {
-            
-            linea = "DDI"; modelo = "150"; precio = 1250;
-
-    }else if (contrapresion <= 10 && caudal >=150 && caudal <=300) {
-           
-            linea = "DDI"; modelo = "300"; precio = 1250;
-
-    }else if (contrapresion <= 10 && caudal >=300 && caudal <=600) {
-      
-            linea = "DDI"; modelo = "600"; precio = 1250;
-
-    }else if (contrapresion <= 10 && caudal >=600 && caudal <=1200){
-        
-        linea = "DDI"; modelo = "DUPLEX"; precio = 1950;  
-
-    }else if (contrapresion <= 10 && caudal >=0.15 && caudal <=1.5) {
-        
-        linea = "Milenio"; modelo = "01510"; precio = 320;
-
-    }else if (contrapresion <= 10 && caudal >=0.30 && caudal <=3) {
-    
-        linea = "Milenio"; modelo = "Milenio 03010"; precio = 320;
-
-    }else if (contrapresion <= 10 && caudal >=0.50 && caudal <=5) {
- 
-        linea = "Milenio"; modelo = "Milenio 05010"; precio = 320;
-
-    }else if (contrapresion > 10 && contrapresion <=50 && caudal <=0.30) {
-
-        linea = "DECI"; modelo = "7/25"; precio = 2200;
-
-    }else if (contrapresion > 10 && contrapresion <=200 && caudal <=0.66){
-        
-        linea = "DECI"; modelo = "10/25"; precio = 2200;
-
-    }else if (contrapresion > 10 && contrapresion <=200 && caudal <=1.85){
-        
-        linea = "DECI"; modelo = "10/70"; precio = 2300;
-
-    }else if (contrapresion > 10 && contrapresion <=200 && caudal <=3){
-        
-        linea = "DECI"; modelo = "15/70"; precio = 2400;
-
-    }else if (contrapresion > 10 && contrapresion <=200 && caudal >=10 && caudal <=25){
-       
-        linea = "DE"; modelo = "20/70"; precio = 2900;
-    }else if (contrapresion > 200 && contrapresion <=800 && caudal <=25){
-    
-        linea = "DEAP"; modelo = "100/800"; precio = 3500;
-    }else{
-        linea = null;
-    }
-    console.log('llegue hasta selector de bombas electricas');
+function selectorDosificadorElectrico() {
+  if (contrapresion <= 10 && caudal >= 16 && caudal <= 30) {
+    linea = "DDI";
+    modelo = "30";
+    precio = 1250;
+  } else if (contrapresion <= 10 && caudal >= 30 && caudal <= 60) {
+    linea = "DDI";
+    modelo = "60";
+    precio = 1250;
+  } else if (contrapresion <= 10 && caudal >= 60 && caudal <= 80) {
+    linea = "DDI";
+    modelo = "80";
+    precio = 1250;
+  } else if (contrapresion <= 10 && caudal >= 80 && caudal <= 150) {
+    linea = "DDI";
+    modelo = "150";
+    precio = 1250;
+  } else if (contrapresion <= 10 && caudal >= 150 && caudal <= 300) {
+    linea = "DDI";
+    modelo = "300";
+    precio = 1250;
+  } else if (contrapresion <= 10 && caudal >= 300 && caudal <= 600) {
+    linea = "DDI";
+    modelo = "600";
+    precio = 1250;
+  } else if (contrapresion <= 10 && caudal >= 600 && caudal <= 1200) {
+    linea = "DDI";
+    modelo = "DUPLEX";
+    precio = 1950;
+  } else if (contrapresion <= 10 && caudal >= 0.15 && caudal <= 1.5) {
+    linea = "Milenio";
+    modelo = "01510";
+    precio = 320;
+  } else if (contrapresion <= 10 && caudal >= 0.3 && caudal <= 3) {
+    linea = "Milenio";
+    modelo = "Milenio 03010";
+    precio = 320;
+  } else if (contrapresion <= 10 && caudal >= 0.5 && caudal <= 5) {
+    linea = "Milenio";
+    modelo = "Milenio 05010";
+    precio = 320;
+  } else if (contrapresion > 10 && contrapresion <= 50 && caudal <= 0.3) {
+    linea = "DECI";
+    modelo = "7/25";
+    precio = 2200;
+  } else if (contrapresion > 10 && contrapresion <= 200 && caudal <= 0.66) {
+    linea = "DECI";
+    modelo = "10/25";
+    precio = 2200;
+  } else if (contrapresion > 10 && contrapresion <= 200 && caudal <= 1.85) {
+    linea = "DECI";
+    modelo = "10/70";
+    precio = 2300;
+  } else if (contrapresion > 10 && contrapresion <= 200 && caudal <= 3) {
+    linea = "DECI";
+    modelo = "15/70";
+    precio = 2400;
+  } else if (
+    contrapresion > 10 &&
+    contrapresion <= 200 &&
+    caudal >= 10 &&
+    caudal <= 25
+  ) {
+    linea = "DE";
+    modelo = "20/70";
+    precio = 2900;
+  } else if (contrapresion > 200 && contrapresion <= 800 && caudal <= 25) {
+    linea = "DEAP";
+    modelo = "100/800";
+    precio = 3500;
+  } else {
+    linea = null;
+  }
+  console.log("llegue hasta selector de bombas electricas");
 }
 
-function selectorDosificadorNeumatico(){
-    console.log('llegue hasta selector de bombas neumaticas')
+function selectorDosificadorNeumatico() {
+  console.log("llegue hasta selector de bombas neumaticas");
 }
 
-function selectorDosificadorSolar(){
-    console.log('llegue hasta selector de bombas solares')
+function selectorDosificadorSolar() {
+  console.log("llegue hasta selector de bombas solares");
 }
