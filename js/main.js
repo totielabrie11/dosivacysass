8b838f1cc6411864ae151bb1ab71b25927dd18bc
totielabrie11 <martinezcarlosmanuel.1@gmail.com>
2022-04-21 21:55:20 -0300

    agrego swhitch para controlar claro oscuro

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-const fondoClaro = 'https://st.depositphotos.com/1012885/4990/i/450/depositphotos_49900577-stock-photo-large-grunge-textures-backgrounds.jpg'
+const d = document
+const btnSwitch = document.querySelector('#switch');
 
-const fondoOscuro = 'https://static6.depositphotos.com/1005574/664/v/450/depositphotos_6644153-stock-illustration-abstract-background.jpg'
-
+btnSwitch.addEventListener('click', () => {
+	let valorDiaNoche = d.body.classList.toggle('dark');
+	 btnSwitch.classList.toggle('active');
+	 
+});
