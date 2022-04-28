7c6a9995c99baa504122ffbbf926612f4c1132d2
totielabrie11 <martinezcarlosmanuel.1@gmail.com>
2022-04-28 18:33:40 -0300

    guardo dark mode en LCS

--- js/main.js
+++ js/main.js
@@ -4,5 +4,21 @@ const btnSwitch = document.querySelector('#switch');
 btnSwitch.addEventListener('click', () => {
 	let valorDiaNoche = d.body.classList.toggle('oscuro');
 	 btnSwitch.classList.toggle('active');
-	 
+	
+	//Guardamos en LCS
+	if (d.body.classList.contains('oscuro')) {
+		localStorage.setItem('dark-mode', 'true')
+	}else{
+		localStorage.setItem('dark-mode', 'false')
+	}
+
 });
+
+//obtenenmos el modo actual
+if (localStorage.getItem('dark-mode') === 'true') {
+	d.body.classList.add('oscuro');	
+	btnSwitch.classList.add('active');
+}else{
+	d.body.classList.remove('oscuro');
+	btnSwitch.classList.remove('active');
+}
