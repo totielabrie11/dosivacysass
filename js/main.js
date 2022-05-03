37982bf63b9483a4ecc600b676af8f2443555224
totielabrie11 <martinezcarlosmanuel.1@gmail.com>
2022-05-03 10:09:13 -0300

    antes de la entrega

--- js/main.js
+++ js/main.js
@@ -145,3 +145,25 @@ $("#BtnModalDeContacto").click( "click", function() {
   $('#contactoContainer').toggle().removeClass('d-none');
 
 });
+
+//////////////////////////// GALERIA 3D ///////////////////////
+var carousel = $(".carousel"),
+    currdeg  = 0;
+
+$(".next").on("click", { d: "n" }, rotate);
+$(".prev").on("click", { d: "p" }, rotate);
+
+function rotate(e){
+  if(e.data.d=="n"){
+    currdeg = currdeg - 60;
+  }
+  if(e.data.d=="p"){
+    currdeg = currdeg + 60;
+  }
+  carousel.css({
+    "-webkit-transform": "rotateY("+currdeg+"deg)",
+    "-moz-transform": "rotateY("+currdeg+"deg)",
+    "-o-transform": "rotateY("+currdeg+"deg)",
+    "transform": "rotateY("+currdeg+"deg)"
+  });
+}
