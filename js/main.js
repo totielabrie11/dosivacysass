c355fcb092942ce53ea33b9ecc46b57456960d2f
totielabrie11 <martinezcarlosmanuel.1@gmail.com>
2022-06-08 15:59:36 -0300

    mejoras en responsive table - slider en contacto

--- js/main.js
+++ js/main.js
@@ -375,10 +375,10 @@ function settingsTools(){
         $("#manualsList").append(
           `        
         <tr>
-            <td>${linea}</td>
+            <td><p>${linea}</p></td>
             <td><img src='${img}' width='100%'></img></td>
-            <td>${tipo}</td>
-            <td><a href="${manualRoot}" download> Descargar</a></td>
+            <td><p>${tipo}</p></td>
+            <td><p><a href="${manualRoot}" download> Descargar</a></p></td>
         </tr>
         
         `
@@ -395,10 +395,10 @@ function settingsTools(){
         $("#repuestoList").append(
           `        
         <tr>
-            <td>${linea}</td>
+            <td><p>${linea}</p></td>
             <td><img src='${img}' width='100%'></img></td>
-            <td>${tipo}</td>
-            <td><a href="${repuestosRoot}" download> Descargar</a></td>
+            <td><p>${tipo}</p></td>
+            <td><p><a href="${repuestosRoot}" download> Descargar</a></p></td>
         </tr>
         
         `
@@ -417,6 +417,52 @@ function settingsTools(){
     })
 }
 
+function settingSlider(){
+  
+  const slider = document.querySelector("#slider");
+  let sliderSection = document.querySelectorAll(".slider__section");
+  let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+  const btnLeft = document.querySelector("#btnLeft");
+  const btnRight = document.querySelector("#btnRight");
+
+  slider.insertAdjacentElement('afterbegin', sliderSectionLast)
+
+  function next(){
+      let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+      slider.style.marginLeft = "-200%";
+      slider.style.transition = "all 1s";
+      setTimeout(function(){
+          slider.style.transition = "none";
+          slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+          slider.style.marginLeft = "-100%";
+      }, 1000);
+  }
+
+  function prev(){
+      let sliderSection = document.querySelectorAll(".slider__section");
+      let sliderSectionLast = sliderSection[sliderSection.length -1];
+      slider.style.marginLeft = "0";
+      slider.style.transition = "all 1s";
+      setTimeout(function(){
+          slider.style.transition = "none";
+          slider.insertAdjacentElement('afterbegin', sliderSectionLast)
+          slider.style.marginLeft = "-100%";
+      }, 1000);
+  }
+
+  btnRight.addEventListener('click', function(){
+      next();
+  });
+
+  btnLeft.addEventListener('click', function(){
+      prev();
+  });
+
+  setInterval(function(){
+      next();
+  }, 5000)
+}
 
 
 
